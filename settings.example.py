e696e605a3a2130c53d0775b1f2e5dc7d9bc03e9
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2011-01-28 03:58:32 +0000

    added CUTYCAPT_COMMAND setting

--- settings.example.py
+++ settings.example.py
@@ -1,2 +1,5 @@
 # The host on which the RabbitMQ server runs
 RABBITMQ_HOST = 'localhost'
+
+# The CutyCapt command to execute
+CUTYCAPT_COMMAND = 'xvfb-run --server-args="-screen 0, 1024x768x24" cutycapt'
