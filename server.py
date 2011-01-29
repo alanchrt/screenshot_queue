a7a80e73b214ea18d66703a0e41718f54cba862c
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2011-01-29 07:21:29 +0000

    wrote the basic server with rabbitmq connectivity

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ to generate website screenshots.
 import os
 import pika
 from webkit2png import WebkitRenderer, init_qtgui
-from settings import RABBITMQ_HOST, SCREENSHOT_ROOT
+from settings import RABBITMQ_HOST, RABBITMQ_QUEUE, SCREENSHOT_ROOT
 
 # Initialize a QApplication
 application = init_qtgui()
@@ -22,7 +22,22 @@ renderer.height = 768
 renderer.timeout = 10
 renderer.format = 'png'
 
-# Take a screenshot
-image = open(os.path.join(SCREENSHOT_ROOT, 'test2.png'), 'w')
-renderer.render_to_file(url='http://stackoverflow.com/', file=image)
-image.close()
+def capture_screenshot(ch, method, properties, body):
+    """Captures a website screenshot."""
+    image = open(os.path.join(SCREENSHOT_ROOT, 'test2.png'), 'w')
+    renderer.render_to_file(url=body, file=image)
+    image.close()
+
+# Open RabbitMQ connection
+connection = pika.AsyncoreConnection(pika.ConnectionParameters(
+        host=RABBITMQ_HOST))
+channel = connection.channel()
+
+# Declare the queue
+channel.queue_declare(queue=RABBITMQ_QUEUE)
+
+# Subscribe to the queue
+channel.basic_consume(capture_screenshot, queue=RABBITMQ_QUEUE, no_ack=True)
+
+# Wait for data
+pika.asyncore_loop()
