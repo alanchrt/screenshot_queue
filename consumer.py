ad446ee2e71f97d016a3329b49d1db011e7cfdd3
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2011-02-02 16:57:29 +0000

    refactored consumer as separate from server daemon

---
+++ consumer.py
@@ -0,0 +1,60 @@
+"""
+screenshot_queue consumer
+=========================
+
+The screenshot_queue consumer listens to a RabbitMQ queue to listen for
+requests to generate website screenshots.
+
+"""
+
+import os
+from pika import (AsyncoreConnection, ConnectionParameters, PlainCredentials,
+                  asyncore_loop)
+from webkit2png import WebkitRenderer, init_qtgui
+from settings import (RABBITMQ_HOST, RABBITMQ_USERNAME, RABBITMQ_PASSWORD,
+                      RABBITMQ_QUEUE, SCREENSHOT_ROOT)
+
+class ScreenshotConsumer():
+    """A consumer for screenshot queue messages."""
+    def __init__(self):
+        # Initialize a QApplication
+        self.application = init_qtgui()
+
+        # Set up the renderer
+        self.renderer = WebkitRenderer()
+        self.renderer.width = 1024
+        self.renderer.height = 768
+        self.renderer.timeout = 10
+        self.renderer.format = 'png'
+
+        # Prepare log file and pid file
+        open('/var/log/screenshot_queue.log', 'a')
+        self.stdin_path = '/var/log/screenshot_queue.log'
+        self.stdout_path = '/var/log/screenshot_queue.log'
+        self.stderr_path = '/var/log/screenshot_queue.log'
+        self.pidfile_path = '/var/run/screenshot_queue.pid'
+        self.pidfile_timeout = 10
+
+    def _capture_screenshot(self, ch, method, properties, body):
+        """Captures a website screenshot."""
+        parameters = body.split()
+        image = open(os.path.join(SCREENSHOT_ROOT, parameters[0]), 'w')
+        self.renderer.render_to_file(url=parameters[1], file=image)
+        image.close()
+
+    def run(self):
+        # Open RabbitMQ connection
+        connection = AsyncoreConnection(ConnectionParameters(
+            host=RABBITMQ_HOST, credentials=PlainCredentials(
+            RABBITMQ_USERNAME, RABBITMQ_PASSWORD)))
+        channel = connection.channel()
+
+        # Declare the queue
+        channel.queue_declare(queue=RABBITMQ_QUEUE)
+
+        # Subscribe to the queue
+        channel.basic_consume(self._capture_screenshot, queue=RABBITMQ_QUEUE,
+                              no_ack=True)
+
+        # Wait for data
+        asyncore_loop()
--- server.py
+++ server.py
@@ -2,44 +2,20 @@
 screenshot_queue server
 =======================
 
-The screenshot_queue server connects to a RabbitMQ queue to listen for requests
-to generate website screenshots.
+Runs the screenshot_queue consumer as a daemon.
+
+Usage
+-----
+
+    python server.py start
+    python server.py stop
+    python server.py restart
 
 """
 
-import os
-from pika import (AsyncoreConnection, ConnectionParameters, PlainCredentials,
-                  asyncore_loop)
-from webkit2png import WebkitRenderer, init_qtgui
-from settings import (RABBITMQ_HOST, RABBITMQ_USERNAME, RABBITMQ_PASSWORD,
-                      RABBITMQ_QUEUE, SCREENSHOT_ROOT)
+from daemon.runner import DaemonRunner
+from consumer import ScreenshotConsumer
 
-# Initialize a QApplication
-application = init_qtgui()
-
-# Set up the renderer
-renderer = WebkitRenderer()
-renderer.width = 1024
-renderer.height = 768
-renderer.timeout = 10
-renderer.format = 'png'
-
-def capture_screenshot(ch, method, properties, body):
-    """Captures a website screenshot."""
-    image = open(os.path.join(SCREENSHOT_ROOT, 'test2.png'), 'w')
-    renderer.render_to_file(url=body, file=image)
-    image.close()
-
-# Open RabbitMQ connection
-connection = AsyncoreConnection(ConnectionParameters(host=RABBITMQ_HOST,
-    credentials=PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)))
-channel = connection.channel()
-
-# Declare the queue
-channel.queue_declare(queue=RABBITMQ_QUEUE)
-
-# Subscribe to the queue
-channel.basic_consume(capture_screenshot, queue=RABBITMQ_QUEUE, no_ack=True)
-
-# Wait for data
-asyncore_loop()
+screenshot_app = ScreenshotConsumer()
+screenshot_daemon = DaemonRunner(screenshot_app)
+screenshot_daemon.do_action()
