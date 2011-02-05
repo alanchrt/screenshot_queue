f1c6f88c530f30d79d0fd64ea7588737b7059aff
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2011-02-05 17:46:49 +0000

    removed daemon mode

--- server.py
+++ server.py
@@ -2,20 +2,16 @@
 screenshot_queue server
 =======================
 
-The screenshot_queue server runs the screenshot_queue consumer as a daemon.
+The screenshot_queue server starts the screenshot_queue consumer loop.
 
 Usage (xvfb optional)
 ---------------------
 
-    xvfb-run --server-args='-screen 0, 1024x768x24' python server.py start
-    xvfb-run --server-args='-screen 0, 1024x768x24' python server.py stop
-    xvfb-run --server-args='-screen 0, 1024x768x24' python server.py restart
+    xvfb-run -a --server-args='-screen 0, 1024x768x24' python server.py &
 
 """
 
-from daemon.runner import DaemonRunner
 from consumer import ScreenshotConsumer
 
-screenshot_app = ScreenshotConsumer()
-screenshot_daemon = DaemonRunner(screenshot_app)
-screenshot_daemon.do_action()
+consumer = ScreenshotConsumer()
+consumer.run()
